8629df62810aa8bae386fe1e514fcdf593ca8632
Maksim <maxamax997gmail.com>
2021-12-19 14:50:42 +0300

    homework №11

---
+++ home work №11/main.js
@@ -0,0 +1,14 @@
+const formArray = [
+  { type: "text", name: "name" },
+  { type: "password", name: "password" },
+  { type: "submit", name: "submit" },
+];
+const form = document.createElement("form");
+
+formArray.forEach((item) => {
+  const input = document.createElement("input");
+  form.append(input);
+  input.innerText = item.name;
+  input.type = item.type;
+});
+document.body.append(form);
