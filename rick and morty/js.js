da1113e9c35e952facb390a1a2a346f1065d2c3a
Maksim <maxamax997gmail.com>
2022-01-20 14:29:15 +0300

    fix cod

--- rick and morty/js.js
+++ rick and morty/js.js
@@ -1,77 +1,80 @@
-async function getResponse() {
-  let response = await fetch("https://rickandmortyapi.com/api/character");
-  let content = await response.json();
-  let allcontent = content["results"];
+async function getResponse(url) {
+  console.log(url);
+  const response = await fetch(url);
 
-  const template = document.querySelector(".template");
+  const content = await response.json();
+  const allcontent = content["results"];
+  const infoallcontent = content["info"];
+  const infoallcontents = infoallcontent.pages;
+  if (pointpagin) {
+    getInfo(infoallcontents);
+  }
 
+  const template = document.querySelector(".content");
   let key;
+  template.innerHTML = " ";
   for (key in allcontent) {
     const episodeLast =
       allcontent[key].episode[allcontent[key].episode.length - 1];
-
     let episodeLastFetch = await fetch(episodeLast);
     let contentEpisodeLastFetch = await episodeLastFetch.json();
 
     const episodeFirst = allcontent[key].episode[0];
     let episodeFirstFetch = await fetch(episodeFirst);
     let contentEpisodeFirstFetch = await episodeFirstFetch.json();
-let className='';
-    if(allcontent[key].status==='Dead'){
-      className='red';
-   }
-   if(allcontent[key].status==='unknown'){
-      className='gray';
-   }
-   
-
+    let className = "";
+    if (allcontent[key].status === "Dead") {
+      className = "red";
+    }
+    if (allcontent[key].status === "unknown") {
+      className = "gray";
+    }
     template.innerHTML += `
-    <div class="content template">
+  
     <div  class="card " >
 <div class="img1">
-               <img src="${allcontent[key].image}" alt="картинка">
+              <img src="${allcontent[key].image}" alt="картинка">
             </div>
             <div class="text">
-               <div>
+              <div>
                   <a class="title" href="#">${allcontent[key].name}</a>
                   <p class=" status ${className}">${allcontent[key].status}  ${
       allcontent[key].status === "unknown" ? "" : "- " + allcontent[key].species
     }</p>
-               </div>
-               <div>
+              </div>
+              <div>
                   <p class="subtitle">Last known location:
                   </p>
                   <a class="last-location-link" href="#">${
                     contentEpisodeLastFetch.name
                   }</a>
-               </div>
-               <div>
+              </div>
+              <div>
                   <p class="subtitle">First seen in:</p>
                   <a class="first-location-link" href="#">${
                     contentEpisodeFirstFetch.name
                   }</a>
-               </div>
+              </div>
             </div>
-            </div>
-            </div>
-`;
+            </div>`;
   }
-  console.log(content);
 }
+let pointpagin = true;
+async function getInfo(pages) {
+  console.log(1)
+  for (let i = 1; i <= pages; i++) {
+    let a = document.createElement("a");
+    a.href = `https://rickandmortyapi.com/api/character?page=${i}`;
+    a.innerText = `${i}`;
+    a.className = "paginA";
+    a.addEventListener("click", async function (event) {
+      event.preventDefault();
+      const url = event.target.href;
+      getResponse(url);
 
-getResponse();
-
-// const cod=fetch(urlric);
-//  (cod)
-// console.log(cod)
-// .then((response) => {
-//  return response.json();
-//})
-//.then((data) => {
-//  const array = Object.entries(data);
-
-// console.log(array)
-//  array.forEach(function (item) {
-
-//  });
-//});
+      pointpagin = false;
+    });
+    pagination.append(a);
+  }
+}
+getResponse("https://rickandmortyapi.com/api/character");
